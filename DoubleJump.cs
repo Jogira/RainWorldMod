e3859dc883c21bc077b12e41304387b60c072bbe
Jonathan <Zweitonegoismus@gmail.com>
2023-03-07 00:30:17 -0500

    Environment setup.

---
+++ DoubleJump.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using System.Security.Permissions;
+//[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace RainWorldMod
+{
+    [BepInPlugin("zweitonegoismus.doublejumpmod", "doublejumpmod", "0.1.0")] // (GUID, mod name, mod version)
+    public class DoubleJump : BaseUnityPlugin
+    {
+
+    }
+}
